28f6563b0be58d4d022ecdde4faf6183fe54f599
Bálint <bogyo0709@gmail.com>
2024-12-19 12:15:00 +0100

    Valami5

--- IKTProjektI_12_SzB_VZ.py
+++ IKTProjektI_12_SzB_VZ.py
@@ -1,5 +1,6 @@
 #Szabó Bálint, Varga Zoltán 10.b, Python első beadandó
-t=[]
+#a feladat
+t=[10, 0, 18, 15, 22, 10, 14, 28, 4, 6, 0, 9]
 n=len(t)
 print(t)
 poz=0
@@ -16,11 +17,14 @@ if(poz>neg):
 if(poz<neg):
     print("Nem igaz, hogy mindig több szállt fel, mint le!")
 
+#b feladat
 tb=[2, 0, 4, 1, 15, 11, 100, 14, 16, 2, 0, 109]
-max=tb[0]
-fel=tb[i]-max
+mall=0
+max=0
 print(tb)
+for i in range(0,2,n):
+    if(max<tb[i]):
+        max=tb[i]
 
 
-        
-print(i,". megállóban voltak a legtöbben!")
+print(t[i],". megállóban voltak a legtöbben!")
