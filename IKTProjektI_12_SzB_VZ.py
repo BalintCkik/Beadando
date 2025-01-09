63b525719c4acdfb9f849c36455e1d9e7b4c8442
Bálint <bogyo0709@gmail.com>
2025-01-09 10:55:10 +0100

    AB kész

--- IKTProjektI_12_SzB_VZ.py
+++ IKTProjektI_12_SzB_VZ.py
@@ -3,22 +3,22 @@ import math
 #Szabó Bálint, Varga Zoltán 10.b, Python első beadandó
 #a feladat
 
-# t=[10, 0, 18, 15, 22, 10, 14, 28, 4, 6, 0, 9]
-# n=len(t)
-# print(t)
-# poz=0
-# neg=0
+t=[10, 0, 18, 15, 22, 10, 14, 28, 4, 6, 0, 9]
+n=len(t)
+print(t)
+poz=0
+neg=0
 
-# for i in range(0,n-1):
-#     for j in range(i+1,n):
-#         if(t[i]<t[j]):
-#             poz+=1
-#         if(t[i]>t[j]):
-#             neg+=1
-# if(poz>neg):
-#     print("Igaz, hogy mindig több szállt fel, mint le!")
-# if(poz<neg):
-#     print("Nem igaz, hogy mindig több szállt fel, mint le!")
+for i in range(0,n-1):
+    for j in range(i+1,n):
+        if(t[i]<t[j]):
+            poz+=1
+        if(t[i]>t[j]):
+            neg+=1
+if(poz>neg):
+    print("Igaz, hogy mindig több szállt fel, mint le!")
+if(poz<neg):
+    print("Nem igaz, hogy mindig több szállt fel, mint le!")
 
 #b feladat
 tb=[2, 0, 4, 1, 15, 11, 100, 14, 16, 2, 0, 109]
